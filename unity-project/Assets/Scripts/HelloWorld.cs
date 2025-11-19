033aff689cc9541ba5c010266f65c7b88df9ed32
Martin Vagstad <martin@dirtybit.no>
2025-11-19 09:02:10 +0100

    added sample script to sample scene. includes intentional compiler warning

---
+++ unity-project/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // This unused variable will produce a compiler warning
+    private int unusedVariable = 42;
+
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+}
